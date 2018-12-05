7f99cbc402f335adc1832d7caeb074c55ca2edb3
boukhemis11 <boukhemis11@gmail.com>
2018-12-05 21:29:47 +0100

    ajouter une fragment dialog pour ajouter les villes

--- app/src/main/java/com/app/boukh/weather/city/CityFragment.kt
+++ app/src/main/java/com/app/boukh/weather/city/CityFragment.kt
@@ -3,22 +3,24 @@ package com.app.boukh.weather.city
 
 import android.os.Bundle
 import android.support.v4.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
+import android.widget.Toast
+import com.app.boukh.weather.App
+import com.app.boukh.weather.Database
 
 import com.app.boukh.weather.R
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- *
- */
 class CityFragment : Fragment() {
+
+    private lateinit var cities: MutableList<City>
+    private lateinit var database: Database
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        database = App.database
+        cities = mutableListOf()
+        setHasOptionsMenu(true)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -28,5 +30,39 @@ class CityFragment : Fragment() {
         return view
     }
 
+    override fun onCreateOptionsMenu(menu: Menu?, inflater: MenuInflater?) {
+        inflater?.inflate(R.menu.fragment_city, menu)
+    }
 
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        when(item?.itemId){
+            R.id.action_create_city -> {
+                showCreacteCityDialog()
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun showCreacteCityDialog() {
+        val createCityFragment = CreateCityDialogFragment()
+        createCityFragment.listener = object : CreateCityDialogFragment.CreateCityDialogListener{
+            override fun onDialogNegativeClick() {
+
+            }
+
+            override fun onDialogPositiveClick(cityName: String) {
+                saveCity(City(cityName))
+            }
+
+        }
+        createCityFragment.show(fragmentManager, "CreateCityDialogFragment")
+    }
+    private fun saveCity(city: City){
+        if (database.createCity(city)) {
+            cities.add(city)
+        }else {
+            Toast.makeText(context, "Could not creat city", Toast.LENGTH_SHORT).show()
+        }
+    }
 }
